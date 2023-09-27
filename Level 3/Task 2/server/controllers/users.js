9f08ba0970d04f27d11613548f463b38594a9557
cluab <cluab11222@gmail.com>
2023-09-27 10:03:48 +0300

    L3T2 backend: Add Users Controllers

---
+++ Level 3/Task 2/server/controllers/users.js
@@ -0,0 +1,78 @@
+import User from '../models/User';
+
+/* READ */
+
+export const getUser = async (res, req) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (!user) return res.status(400).json({ msg: 'User does not exist. ' });
+    res.status(200).json({ user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const getUserFriends = async (res, req) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+
+    if (!user) return res.status(400).json({ msg: 'User does not exist. ' });
+
+    const friends = await Promise.all(
+      user.friends.map((id) => {
+        User.findById(id);
+      })
+    );
+
+    const formattedFriends = friends.map(
+      (_id, firstName, lastName, occupation, location, picturePath) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+
+    res.status(200).json(formattedFriends);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+/* UPDATE */
+
+export const addRemoveFriend = async () => {
+  try {
+    const { id, friendId } = req.params;
+    const user = await User.findById(id);
+    const friend = await User.findById(friendId);
+
+    if (!user) return res.status(400).json({ msg: 'User does not exist. ' });
+    if (!friend)
+      return res.status(400).json({ msg: 'friend does not exist. ' });
+
+    if (user.friends.includes(friendId)) {
+      user.friends = user.friends.filter((id) => id !== friendId);
+      friend.friends = friend.friends.filter((id) => id !== id);
+    } else {
+      user.friends.push(friendId);
+      friend.friends.push(id);
+    }
+    await user.save();
+    await friend.save();
+
+    const friends = await Promise.all(
+      user.friends.map((id) => {
+        User.findById(id);
+      })
+    );
+
+    const formattedFriends = friends.map(
+      (_id, firstName, lastName, occupation, location, picturePath) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+    res.status(200).json(formattedFriends);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
