40e08399f18e5b6702aec469d2b17195f6ef2dea
cluab <cluab11222@gmail.com>
2023-09-28 18:10:37 +0300

    L3T2 backend: Add Post Routes

--- Level 3/Task 2/server/index.js
+++ Level 3/Task 2/server/index.js
@@ -10,7 +10,10 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { register } from './controllers/auth.js';
 import authRouter from './routes/auth.js';
-import usersRouter from './routes/users.js'
+import usersRouter from './routes/users.js';
+import postsRouter from './routes/posts.js';
+import { createPost } from './controllers/posts.js';
+import { verifyToken } from './middleware/auth.js';
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -39,10 +42,12 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post('/auth/register', upload.single('picture'), register);
+app.post('/posts', verifyToken, upload.single('picture'), createPost);
 
 /* ROUTES */
 app.use('/auth', authRouter);
-app.use('/users', usersRouter)
+app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
